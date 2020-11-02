cf36053584aa6ee56120e3742490bb5a6c8887a7
BBEdge <kryamov@mail.ru>
2020-11-02 10:39:08 +0300

    Initial commit

--- initdb.py
+++ initdb.py
@@ -4,13 +4,12 @@ import sqlite3
 def init_db():
     global dbconn
     ''' init database and connection '''
-    DATABASE = 'test.db'
+    #DATABASE = 'test.db'
+    DATABASE = ':memory:'
 
     try:
         dbconn = sqlite3.connect(DATABASE)
         #dbconn.row_factory = sqlite3.Row
-        #DATABASE = ":memory:"
-        #dbconn = sqlite3.connect(DATABASE)
 
         with open('static/sql/schema.sql') as f:
             dbconn.executescript(f.read())
